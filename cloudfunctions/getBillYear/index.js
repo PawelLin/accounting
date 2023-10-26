3590148eb76f0921a367b2f479a20dc900889724
pawellin <pawellin@foxmail.com>
2023-10-26 23:21:13 +0800

    feat: 新增云函数getBillYear

---
+++ cloudfunctions/getBillYear/index.js
@@ -0,0 +1,35 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
+
+const db = cloud.database()
+const $ = db.command.aggregate
+// 云函数入口函数
+exports.main = async (event, context) => {
+    const { year, type } = event
+    const { OPENID } = cloud.getWXContext()
+    const result = await db.collection('bill').aggregate().limit(1000).match({
+        date: db.RegExp({ regexp: year }),
+        type,
+        openid: OPENID
+    }).addFields({
+        month: $.substr(['$date', 0, 7])
+    }).group({
+        _id: {
+            label: '$label',
+            labelTitle: '$labelTitle',
+            type: '$type',
+            month: '$month'
+        },
+        amount: $.sum('$amount')
+    }).project({
+        _id: 0,
+        label: '$_id.label',
+        labelTitle: '$_id.labelTitle',
+        type: '$_id.type',
+        date: '$_id.month',
+        amount: 1
+    }).end()
+    return result
+}
---
+++ miniprogram/utils/cloud/mock/getBillYear.js
@@ -0,0 +1,36 @@
+import { payLabels, incomeLabels } from './getLabel'
+
+export default params => {
+    const year = params.year
+    return {
+        result: {
+            data: [
+                ...Array.from(Array(100)).map((item, index) => {
+                    const labelIndex = Math.floor(Math.random() * payLabels.length) + 1
+                    const { label, labelTitle } = payLabels[labelIndex - 1]
+                    const month = Math.ceil(Math.random() * 12).toString().padStart(2, 0)
+                    return {
+                        amount: Number((Math.random() * labelIndex ** 3).toFixed(2)),
+                        date: `${year}-${month}`,
+                        label,
+                        labelTitle,
+                        type: '0'
+                    }
+                }),
+                ...incomeLabels.length ? Array.from(Array(100)).map((item, index) => {
+                    const labelIndex = Math.floor(Math.random() * incomeLabels.length) + 1
+                    const { label, labelTitle } = incomeLabels[labelIndex - 1]
+                    const month = Math.ceil(Math.random() * 12).toString().padStart(2, 0)
+                    return {
+                        amount: Number((Math.random() * labelIndex ** 3).toFixed(2)),
+                        date: `${year}-${month}`,
+                        label,
+                        labelTitle,
+                        type: '1'
+                    }
+                }) : []
+            ],
+        },
+        requestID: 'requestID'
+    }
+}
